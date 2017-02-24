d7675ee58b6c70617284198bcd1fb965c89c18be
Sam Agnew <sagnew@eden.rutgers.edu>
2017-02-24 16:12:28 -0500

    Add Xcode project

---
+++ BeerGIFs/BeerGIFs/GiphyManager.swift
@@ -0,0 +1,9 @@
+//
+//  GiphyManager.swift
+//  BeerGIFs
+//
+//  Created by Sam Agnew on 2/24/17.
+//  Copyright © 2017 Sam Agnew. All rights reserved.
+//
+
+import Foundation
---
+++ BeerGIFs/BeerGIFs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerGIFs
+//
+//  Created by Sam Agnew on 2/24/17.
+//  Copyright © 2017 Sam Agnew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
