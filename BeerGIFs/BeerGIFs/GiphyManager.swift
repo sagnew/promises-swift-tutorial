c843c9832b21f7aa06b82bd24f6cb8b2569247b1
Sam Agnew <sagnew@eden.rutgers.edu>
2017-02-28 11:40:00 -0500

    Update GiphyManager.swift

--- BeerGIFs/BeerGIFs/GiphyManager.swift
+++ BeerGIFs/BeerGIFs/GiphyManager.swift
@@ -19,6 +19,7 @@ class GiphyManager: NSObject {
   let imageLimit: UInt32
   
   override init() {
+    // The default public API key
     self.apiKey = "dc6zaTOxFJmzC"
     self.imageLimit = 50
     super.init()
@@ -30,17 +31,19 @@ class GiphyManager: NSObject {
     super.init()
   }
   
-  func fetchRandomGifUrl(forSearchQuery query: String) -> Promise<String> { //completionHandler: @escaping (_ forImageUrl: String) -> Void) {
+  func fetchRandomGifUrl(forSearchQuery query: String) -> Promise<String> {
+    let params = ["api_key": self.apiKey, "q": query, "limit": "\(self.imageLimit)"]
+    
     return Promise { fulfill, reject in
-      Alamofire.request(self.giphyBaseURL, parameters: ["api_key": self.apiKey, "q": query, "limit": "\(self.imageLimit)"])
+      Alamofire.request(self.giphyBaseURL, parameters: params)
         .responseJSON { response in
           if let result = response.result.value {
             let json = JSON(result)
-            let randomNum:Int = Int(arc4random_uniform(self.imageLimit))
+            let randomNum = Int(arc4random_uniform(self.imageLimit))
             
             if let imageUrlString = json["data"][randomNum]["images"]["downsized"]["url"].string {
               print(imageUrlString)
-              fulfill(imageUrlString) // completionHandler(imageUrlString)
+              fulfill(imageUrlString)
             } else {
               reject(response.error!)
             }
