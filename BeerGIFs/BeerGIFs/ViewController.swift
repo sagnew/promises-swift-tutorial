325671fe4353086858f3ab389c7f80262f33f5af
Sam Agnew <sagnew@eden.rutgers.edu>
2017-02-28 11:40:29 -0500

    Fix errors with displaying GIFs

--- BeerGIFs/BeerGIFs/ViewController.swift
+++ BeerGIFs/BeerGIFs/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import PromiseKit
+import SwiftyGif
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var beerGifImageView: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    let giphy = GiphyManager()
+    
+    giphy.fetchRandomGifUrl(forSearchQuery: "SNES").then { imageUrlString in
+      self.fetchImage(forImageUrl: imageUrlString)
+    }.then { imageData in
+      self.attachImage(withImage: imageData)
+    }.catch { error in
+        print(error)
+    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +34,24 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func fetchImage(forImageUrl imageUrlString: String) -> Promise<Data> {
+    return Promise { fulfill, reject in
+      Alamofire.request(imageUrlString).responseData { response in
+        if let imageData = response.result.value {
+          print("image downloaded")
+          fulfill(imageData)
+        } else {
+          reject(response.error!)
+        }
+      }
+    }
+  }
+  
+  func attachImage(withImage imageData: Data) -> Void {
+    print("Displaying image")
+    let image = UIImage(gifData: imageData)
+    self.beerGifImageView.setGifImage(image)
+  }
 
 }
 
