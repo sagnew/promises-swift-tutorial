5eb06ed0995969e30c253d327650990a584bcff8
Sam Agnew <sagnew@eden.rutgers.edu>
2017-03-01 17:52:43 -0500

    Add comments

--- SNESGifs/SNESGifs/GiphyManager.swift
+++ SNESGifs/SNESGifs/GiphyManager.swift
@@ -32,19 +32,29 @@ class GiphyManager: NSObject {
   }
   
   func fetchRandomGifUrl(forSearchQuery query: String) -> Promise<String> {
+    
+    // Parameters for the Giphy API request.
     let params = ["api_key": self.apiKey, "q": query, "limit": "\(self.imageLimit)"]
     
+    // Return a Promise for the caller of this function to use.
     return Promise { fulfill, reject in
+      
+      // Make an HTTP request to the Giphy API.
       Alamofire.request(self.giphyBaseURL, parameters: params)
         .responseJSON { response in
           if let result = response.result.value {
+            
+            // Parse the JSON response from the API request.
             let json = JSON(result)
+            
+            // Generate a random number to determine which GIF to use.
             let randomNum = Int(arc4random_uniform(self.imageLimit))
             
             if let imageUrlString = json["data"][randomNum]["images"]["downsized"]["url"].string {
-              print(imageUrlString)
+              // Pass the URL of this image to the next function call in the chain.
               fulfill(imageUrlString)
             } else {
+              // Reject the Promise if something went wrong.
               reject(response.error!)
             }
             
