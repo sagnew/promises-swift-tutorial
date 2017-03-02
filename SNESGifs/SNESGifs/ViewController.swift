f4e10e6a85fe37b6fb521e32eab431702a9c28c6
Sam Agnew <sagnew@eden.rutgers.edu>
2017-03-01 19:19:55 -0500

    Revert code to tutorial version

--- SNESGifs/SNESGifs/ViewController.swift
+++ SNESGifs/SNESGifs/ViewController.swift
@@ -18,50 +18,11 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let giphy = GiphyManager()
-    
-    giphy.fetchRandomGifUrl(forSearchQuery: "SNES").then { imageUrlString in
-      self.fetchImage(forImageUrl: imageUrlString)
-    }.then { imageData in
-      self.attachImage(withImageData: imageData)
-    }.catch { error in
-      print(error)
-    }
-    
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-  func fetchImage(forImageUrl imageUrlString: String) -> Promise<Data> {
-    
-    // Return a Promise for the caller of this function to use.
-    return Promise { fulfill, reject in
-      
-      // Make an HTTP request to download the image.
-      Alamofire.request(imageUrlString).responseData { response in
-        
-        if let imageData = response.result.value {
-          print("image downloaded")
-          
-          // Pass the image data to the next function.
-          fulfill(imageData)
-        } else {
-          
-          // Reject the Promise if something went wrong.
-          reject(response.error!)
-        }
-      }
-    }
-  }
-  
-  func attachImage(withImageData imageData: Data) -> Void {
-    print("Displaying image")
-    let image = UIImage(gifData: imageData)
-    self.snesGifImageView.setGifImage(image)
-  }
-
 }
 
